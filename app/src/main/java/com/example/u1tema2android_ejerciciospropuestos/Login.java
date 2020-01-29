67a402a09198448b3c9ae8cde0bf1f6aa4bd2e1c
Ronald Ordoñez <37679541+ronordonezq@users.noreply.github.com>
2020-01-29 00:51:41 -0500

    login y permisos

--- app/src/main/java/com/example/u1tema2android_ejerciciospropuestos/Login.java
+++ app/src/main/java/com/example/u1tema2android_ejerciciospropuestos/Login.java
@@ -1,8 +1,19 @@
 package com.example.u1tema2android_ejerciciospropuestos;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +21,134 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+
+
+    public void solicitarPermiso(final String permiso, String justificacion, final int codigo) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permiso)) {
+            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+            dialogo1.setTitle("Solicitud de permiso");
+            dialogo1.setMessage(justificacion);
+            dialogo1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                    ActivityCompat.requestPermissions(Login.this, new String[]{permiso}, codigo);
+                }
+            });
+            dialogo1.show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{permiso}, codigo);
+        }
+    }
+
+    void permisocamara(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            arranque();
+        } else {
+            solicitarPermiso(Manifest.permission.CAMERA,
+                    "Sin el permiso" + " no podremos acceder a la cámara", 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        if (requestCode == 0) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                permisocamara();
+            } else {
+                solicitarPermiso(Manifest.permission.ACCESS_FINE_LOCATION,
+                        "Sin el permiso" + " de ubicacion no podremos localizarte", 0);
+            }
+        }
+        if (requestCode == 1) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                arranque();
+            } else {
+                solicitarPermiso(Manifest.permission.CAMERA,
+                        "Sin el permiso" + " no podremos acceder a la cámara", 1);
+            }
+        }
+    }
+    void arranque() {
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle("FELICITACIONES");
+        dialogo1.setMessage("Usted ya tiene los permisos necesarios para usar nuestra app");
+        dialogo1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                finish();
+            }
+        });
+        dialogo1.show();
+    }
+
+    public void navegacion(View view) {
+        if ((ContextCompat.checkSelfPermission(Login.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) ){
+            arranque();
+        } else {
+            Handler handler = new Handler();
+            handler.postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            if (ContextCompat.checkSelfPermission(Login.this,
+                                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                                permisocamara();
+                            } else {
+                                solicitarPermiso(Manifest.permission.ACCESS_FINE_LOCATION,
+                                        "Sin el permiso" + " de ubicacion no podremos localizarte", 0);
+                            }
+                        }
+                    }, 2000L);
+        }
+
+
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setMessage("Seleccione la aplicación");
+        builder.setTitle("Escoja entre Waze o Google Maps");
+        builder.setNeutralButton("Google Map", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Uri gmmIntentUri;
+                gmmIntentUri = Uri.parse("google.navigation:q=" + -18.004869 + "," + -70.234808);
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null)
+                    startActivity(mapIntent);
+                else
+                    Toast.makeText(Login.this, "Maps no esta instalado", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("Waze", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Uri gmmIntentUri;
+                gmmIntentUri = Uri.parse("waze://?ll=" + -18.004869 + "," + -70.234808 + "&navigate=yes");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.waze");
+                if (mapIntent.resolveActivity(getPackageManager()) != null)
+                    startActivity(mapIntent);
+                else
+                    Toast.makeText(Login.this, "Waze no esta instalado", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setPositiveButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                return;
+            }
+        });
+        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                return;
+            }
+        });
+        builder.show();
+    }
+
+    public void Registrar(View view) {
+        startActivity(new Intent(this, Registrar.class));
     }
 }
